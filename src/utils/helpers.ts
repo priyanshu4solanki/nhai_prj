3b8ba241091381eb5de2af11732bba8fdc944c55
ansharyan007 <ansharyan356@gmail.com>
2026-05-31 14:05:30 +0530

    Update helpers.ts

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,7 +1,9 @@
-import { v4 as uuidv4 } from 'uuid';
-
 export const generateUUID = (): string => {
-  return uuidv4();
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, (c) => {
+    const r = Math.floor(Math.random() * 16);
+    const v = c === 'x' ? r : (r & 0x3) | 0x8;
+    return v.toString(16);
+  });
 };
 
 export const getCurrentTimestamp = (): number => {
